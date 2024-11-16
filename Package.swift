5ada3428882ada03db5cf48c8e3f5ec8484b189d
Nguyen Tuan Anh <104725942+tuanemdev@users.noreply.github.com>
2024-11-16 20:11:09 +0700

    String + Ext

--- Package.swift
+++ Package.swift
@@ -6,14 +6,11 @@ import PackageDescription
 let package = Package(
     name: "SwiftExtensions",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftExtensions",
             targets: ["FoundationExtensions", "UIKitExtensions", "SwiftUIExtensions"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "FoundationExtensions"),
         .target(
--- Sources/FoundationExtensions/String+Extensions.swift
+++ Sources/FoundationExtensions/String+Extensions.swift
@@ -9,4 +9,25 @@ public extension String {
     subscript(index: Int) -> Character {
         self[self.index(startIndex, offsetBy: index)]
     }
+    
+    subscript(safe index: Int) -> Character? {
+        guard index >= 0, index < count else { return nil }
+        return self[self.index(startIndex, offsetBy: index)]
+    }
+    
+    var rawRepresentable: Data {
+        Data(self.utf8)
+    }
+    
+    var base64Encoded: String {
+        self.rawRepresentable.base64EncodedString()
+    }
+    
+    func localized(tableName: String? = nil, bundle: Bundle = Bundle.main, value: String = "", comment: String = "") -> String {
+        return NSLocalizedString(self, tableName: tableName, bundle: bundle, value: value, comment: comment)
+    }
+    
+    func format(_ arguments: any CVarArg...) -> String {
+        return String(format: self, arguments: arguments)
+    }
 }
