da12ee2893eaf395cc7c4f672d946b2c08ead7ed
Nguyen Tuan Anh <104725942+tuanemdev@users.noreply.github.com>
2024-11-17 01:21:00 +0700

    update removeduplicates

--- Sources/FoundationExtensions/Array+Extensions.swift
+++ Sources/FoundationExtensions/Array+Extensions.swift
@@ -6,19 +6,8 @@
 import Foundation
 
 public extension Array where Element: Hashable {
-    func removeDuplicates() -> (uniqueElements: [Element], removedElements: [Element]) {
+    func removeDuplicates() -> Self {
         var seen = Set<Element>()
-        var uniqueElements = [Element]()
-        var removedElements = [Element]()
-        
-        self.forEach {
-            if seen.insert($0).inserted {
-                uniqueElements.append($0)
-            } else {
-                removedElements.append($0)
-            }
-        }
-        
-        return (uniqueElements, removedElements)
+        return self.filter { seen.insert($0).inserted }
     }
 }
